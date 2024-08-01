90e4428f6ae60dfd518db60b3d9ece7720b1d75e
VeRoyter <137034950+VeRoyter@users.noreply.github.com>
2024-08-01 20:44:31 +1000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Список товаров магазина "ПЯТЕРОЧКА"</h1>
+    <Products />
+  </div>
+</template>
+
+<script>
+import Products from './views/Products.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Products,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #39bd6c;
+  margin-top: 100px;
+}
+
+h1 {
+  color: #bb3939;
+}
+</style>
